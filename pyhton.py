481efb90df6893f46b1934126a65179690c3cf68
rahulnalathati-21 <rahulsiddharth2.0@gmail.com>
2026-03-16 16:22:34 +0530

    Update pyhton.py

--- pyhton.py
+++ pyhton.py
@@ -1,14 +1,1 @@
-x = "programming language"
 
-def myfun():
-    print ("pyhton is " + x)
-
-myfun()
-
-##################################################################
-
-def myfun():
-    x = "procedural language"
-    print ("pyhton is " + x)
-
-myfun()    
